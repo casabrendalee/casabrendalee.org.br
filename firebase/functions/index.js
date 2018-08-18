d56fca30deb0a397cd71b7015f98b8ca07a495cf
Matheus Pereira <matheus.spereira@hotmail.com>
2018-08-17 21:29:41 -0300

    new email

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,14 +3,14 @@ const functions  = require('firebase-functions');
 const nodemailer = require('nodemailer');
 const cors = require('cors')({origin: true});
 
-let url = `smtps://site.casabrendalee%40gmail.com:${encodeURIComponent('sergio#2018')}@smtp.gmail.com:465`;
+let url = `smtps://site.casabrendalee%40gmail.com:${encodeURIComponent('SENHA')}@smtp.gmail.com:465`;
 let transporter = nodemailer.createTransport(url);
 
 exports.enviarEmail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     let email = {
         from: '"Site Casa Brenda Lee" <site.casabrendalee@gmail.com>',
-        to: 'site.casabrendalee@gmail.com',
+        to: 'site.casabrendalee@gmail.com,adm.casabrendalee@gmail.com',
         subject: req.body['subject'],
         html: req.body['content']
     };
